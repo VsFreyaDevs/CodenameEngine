f32d5e0410d9c127f44001cb5f4e20ab8a1bf1d2
Microkat <89645987+THEkatinamicrowave@users.noreply.github.com>
2025-02-09 19:44:44 -0600

    Create EnterPressedEvent
    i love zingers

---
+++ source/funkin/backend/scripting/events/EnterPressedEvent.hx
@@ -0,0 +1,30 @@
+package funkin.backend.scripting.events;
+
+import funkin.backend.utils.CoolUtil.CoolSfx;
+
+final class EnterPressedEvent extends CancellableEvent {
+  /**
+   * Whether the screen should flash on PRESS ENTER TO START
+   */
+  public var flash:Bool;
+
+    /**
+   * The color that the screen flashes
+   */
+  public var flashColor:FlxColor;
+
+    /**
+   * Whether PRESS ENTER TO START should play a menu sfx
+   */
+  public var playSfx:Bool;
+
+    /**
+   * The menu sound to play
+   */
+  public var menuSound:CoolSfx;
+
+    /**
+   * The time between enter being pressed and the transition into MainMenuState
+   */
+  public var timerLength:Float;
+}
