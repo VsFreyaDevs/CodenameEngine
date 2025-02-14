899f9e4452603915249120b18f29ad6fcf256d06
Microkat <89645987+THEkatinamicrowave@users.noreply.github.com>
2025-02-14 11:17:49 -0500

    make onSongEnd cancellable
    idk some rando asked for it

--- source/funkin/game/PlayState.hx
+++ source/funkin/game/PlayState.hx
@@ -1479,7 +1479,9 @@ class PlayState extends MusicBeatState
 	 */
 	public function endSong():Void
 	{
-		scripts.call("onSongEnd");
+		var event = scripts.event("onSongEnd", new CancellableEvent());
+		if (event.cancelled) return;
+		
 		canPause = false;
 		inst.volume = 0;
 		vocals.volume = 0;
