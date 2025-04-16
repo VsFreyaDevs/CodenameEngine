6dc908b69571f39446ec227f5abb1a32a2880cee
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2025-04-16 16:04:19 -0400

    Script call when setting Downscroll.
    Lets say your HUD needed some changes when downscroll was enabled. This script call would help you be able to still switch downscroll on the fly without fucking up your HUD in the process!

--- source/funkin/game/PlayState.hx
+++ source/funkin/game/PlayState.hx
@@ -137,8 +137,13 @@ class PlayState extends MusicBeatState
 	 */
 	public var downscroll(get, set):Bool;
 
-	@:dox(hide) private function set_downscroll(v:Bool) {return camHUD.downscroll = v;}
-	@:dox(hide) private function get_downscroll():Bool  {return camHUD.downscroll;}
+	@:dox(hide) private function set_downscroll(v:Bool) {
+		scripts.call('onDownscrollSet', [v]);
+		return camHUD.downscroll = v;
+	}
+	@:dox(hide) private function get_downscroll():Bool {
+		return camHUD.downscroll;
+	}
 
 	/**
 	 * Instrumental sound (Inst.ogg).
