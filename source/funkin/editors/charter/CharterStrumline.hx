6cd8a6bd9ad0c887199374d4d33df87c2e3af932
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-21 16:39:42 -0400

    base cne changes and stuffs

--- source/funkin/editors/charter/CharterStrumline.hx
+++ source/funkin/editors/charter/CharterStrumline.hx
@@ -43,23 +43,7 @@ class CharterStrumline extends UISprite {
 
 		if(strumLine.visible == null) strumLine.visible = true;
 
-		var icons = strumLine.characters != null ? strumLine.characters : [];
-
 		healthIcons = new FlxSpriteGroup(x, y);
-
-		var maxCol = icons.length < 4 ? icons.length : 4;
-		var maxRow = Math.floor((icons.length-1) / 4) + 1;
-		for (i=>icon in icons) {
-			var healthIcon = new HealthIcon(Character.getIconFromCharName(icon));
-			var newScale = Math.max((0.6 - (icons.length / 20)), 0.35);
-			healthIcon.scale.x = healthIcon.scale.y = healthIcon.defaultScale * newScale;
-			healthIcon.updateHitbox();
-
-			healthIcon.x = FlxMath.lerp(0, Math.min(icons.length * 20, 120), (maxCol-1 != 0 ? (i % 4) / (maxCol-1) : 0));
-			healthIcon.y = (draggable ? 29 : 7) + FlxMath.lerp(0, Math.min(maxRow * 15, 60), (maxRow-1 != 0 ? Math.floor(i / 4) / (maxRow-1) : 0));
-			healthIcon.alpha = strumLine.visible ? 1 : 0.4;
-			healthIcons.add(healthIcon);
-		}
 
 		members.push(healthIcons);
 
@@ -74,8 +58,7 @@ class CharterStrumline extends UISprite {
 		button = new CharterStrumlineOptions(this);
 		members.push(button);
 
-		vocals = strumLine.vocalsSuffix.length > 0 ? FlxG.sound.load(Paths.voices(PlayState.SONG.meta.name, PlayState.difficulty, strumLine.vocalsSuffix)) : new FlxSound();
-		vocals.group = FlxG.sound.defaultMusicGroup;
+		updateInfo();
 
 		selectedWaveform = -1;
 	}
@@ -113,7 +96,8 @@ class CharterStrumline extends UISprite {
 		for (i=>icon in icons) {
 			var healthIcon = new HealthIcon(Character.getIconFromCharName(icon));
 			var newScale = Math.max((0.6 - (icons.length / 20)), 0.35);
-			healthIcon.scale.x = healthIcon.scale.y = healthIcon.defaultScale * newScale;
+			var size = Std.int(150 * newScale);
+			healthIcon.setUnstretchedGraphicSize(size, size, true);
 			healthIcon.updateHitbox();
 
 			healthIcon.x = FlxMath.lerp(0, Math.min(icons.length * 20, 120), (maxCol-1 != 0 ? (i % 4) / (maxCol-1) : 0));
